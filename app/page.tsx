4d2d1750c8a8d933580c30968f42c3872e953ab9
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2024-12-22 13:24:19 -0500

    Fix import path in page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { Quiz } from '@/components/quiz/Quiz';
+import { Quiz } from './components/quiz/Quiz';
 
 export default function Home() {
   const [started, setStarted] = useState(false);
