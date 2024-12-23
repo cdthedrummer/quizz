6203e1f8f2f35e017efd6dab757ca09ec9372619
cdthedrummer <113496461+cdthedrummer@users.noreply.github.com>
2024-12-22 23:07:29 -0500

    Fix Tailwind configuration and globals.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,12 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+      },
+    },
   },
   plugins: [],
 };
